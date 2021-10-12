bdc3e2e08b3ef97f6aa2773e88166171f1e30769
Adrian <adrian.loz@hotmail.com>
2021-10-12 12:15:30 +0200

    práctica vacía

---
+++ soluciones/cajeras-executor/ALozano/src/main/java/org/alozano/App.java
@@ -0,0 +1,14 @@
+package org.alozano;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
