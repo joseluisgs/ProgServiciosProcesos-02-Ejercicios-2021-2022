979a43e70cb1bf989ff73317e594701108c97da6
Eneko Rebollo <eneko12rebollo@gmail.com>
2021-10-14 22:51:49 +0200

    commit final con todo lo que se pide

--- soluciones/cajeras-executor/ERebollo/CajerasExecuter/src/main/java/Cliente.java
+++ soluciones/cajeras-executor/ERebollo/CajerasExecuter/src/main/java/Cliente.java
@@ -4,23 +4,16 @@ import lombok.Data;
 import java.util.Stack;
 
 @Data
-@AllArgsConstructor
 public class Cliente {
 
     private int clientNumber;
-    private final int MAX_ITEM_AMOUNT =30;
-    private int cartMaxItems = (int)(Math.random() * MAX_ITEM_AMOUNT)+1;
-    private Stack<Producto> cart = new Stack<>();
+    private Stack<Producto> carro = new Carrito().getCart();
 
     public Cliente(int numero){
         this.clientNumber=numero;
-        fillCart();
     }
 
-    private void fillCart(){
-        for(int i=0;i<MAX_ITEM_AMOUNT;i++)
-            cart.add(new Producto((int) (Math.random() * 5) + 1));
-    }
+
 
 
 
--- soluciones/cajeras-executor/ERebollo/CajerasExecuter/src/main/java/Main.java
+++ soluciones/cajeras-executor/ERebollo/CajerasExecuter/src/main/java/Main.java
@@ -7,10 +7,38 @@ import java.util.concurrent.Executors;
 @Data
 public class Main {
 
-    private static ArrayDeque<Cliente> queue =  new ArrayDeque<Cliente>();
+    private static ArrayDeque<Cliente> queue;
+    private static Caja[] cajas;
+    private static Long totalTimeSecuencial;
+    private static Long totalTimeConcurrente;
 
-    private static void startUp(int numeroClientes, int hilos){
-        addClients(numeroClientes);
+    public static void startUp(int clientes, int numCajas){
+        queue = new Clientes(clientes).getClientes();
+
+        long initialTime = System.currentTimeMillis();
+
+        startUpSecuencial(clientes,numCajas,initialTime);
+        startUpConcurrente(clientes,numCajas,initialTime);
+    }
+
+    private static void startUpSecuencial(int clientes, int numCajas, long initialTime){
+
+        cajas = new Caja[numCajas];
+
+        while(!queue.isEmpty()){
+            for(int i=0;i<numCajas;i++){
+                if(!queue.isEmpty()){
+                    cajas[i]=new Caja(queue.getFirst(),i);
+                    queue.removeFirst();
+                    cajas[i].run();
+                }
+            }
+        }
+        totalTimeSecuencial = (System.currentTimeMillis()-initialTime)/1_000;
+    }
+
+    private static void startUpConcurrente(int numeroClientes, int hilos, long initialTime){
+
         ExecutorService service = Executors.newFixedThreadPool(hilos);
         int numero=1;
 
@@ -22,18 +50,20 @@ public class Main {
         }
 
         service.shutdown();
-    }
-
-    private static void addClients(int numeroClientes){
-       for(int i=0;i<numeroClientes;i++){
-           queue.add(new Cliente(i));
-       }
+        totalTimeConcurrente = (System.currentTimeMillis()-initialTime)/1_000;
     }
 
     public static void main(String[] args) {
-        final int HILOS = Integer.parseInt(args[0]);
-        final int CLIENTES = Integer.parseInt(args[1]);
+        try {
+            final int HILOS = Integer.parseInt(args[0]);
+            final int CLIENTES = Integer.parseInt(args[1]);
 
-        startUp(CLIENTES, HILOS);
+            startUp(CLIENTES, HILOS);
+            System.out.println("Tiempo necesario de forma secuencial: "+totalTimeSecuencial);
+            System.out.println("Tiempo necesario de forma concurrente: "+totalTimeConcurrente);
+
+        }catch(NumberFormatException e){
+            System.out.println("Parametros no validos");
+        }
     }
 }
